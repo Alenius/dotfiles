0011eda45977a49f9b85bb74cb9dc5c59974af44
Adam Alenius <adam.alenius@northvolt.com>
2022-07-07 18:48:33 +0200

    add hammerspoon date with underscores

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,4 +2,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "i", function()
   hs.eventtap.keyStrokes(os.date("%c"))
 end)
 
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "j", function()
+  hs.eventtap.keyStrokes(os.date("%Y_%m_%d"))
+end)
 
